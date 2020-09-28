0af954211447e3171bb22cc3f59b5a8bad1457d0
angel-AI-TG <71563327+angel-AI-TG@users.noreply.github.com>
2020-09-28 22:52:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1181932534:AAERzGjycRYaL1nHjcKJB60649WNicd01Ks"
 
 def download(bot, update):
     message = update.effective_message
